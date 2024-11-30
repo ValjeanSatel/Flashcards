421b3c004aa8d126baca980c06a56159934a45de
ValjeanSatel <00001106485312sp@al.educacao.sp.gov.br>
2024-11-30 19:01:11 -0300

    Making final adjusts.

---
+++ script.js
@@ -0,0 +1,42 @@
+criaCartao (
+    "Geografia",
+    "Quais países fazem parte do Reino Unido?",
+    "Inglaterra, Escócia, País de Gales e Irlanda do Norte"
+)
+
+criaCartao (
+    "Física",
+    "O que diz a 2ª lei de Newton?",
+    "A força resultante que age sobre um corpo é igual ao produto da massa do corpo pela sua aceleração"
+)
+
+criaCartao (
+    "Matemática",
+    "Quanto é 512 vezes 23?",
+    "11776"
+)
+
+criaCartao (
+    "Geografia",
+    "Qual é a capital de Senegal?",
+    "A capital é Dacar"
+) 
+
+criaCartao (
+    "Programação",
+    "O que é CSS?",
+    "É uma linguagem de estilização"
+)
+
+criaCartao (
+    "Português",
+    "Quais as formas nominais do verbo?",
+    "Infinitivo, gerúndio e particípio"
+)
+
+criaCartao (
+    "Inglês",
+    "In Celsius, what temperature does water boil?",
+    "At 100°"
+)
+
